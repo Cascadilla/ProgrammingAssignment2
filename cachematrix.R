2bd65bc82e1dc4ffd0c9b5e825d8a9774e17d352
Cascadilla <ithaca@live.com>
2014-05-24 17:54:14 -0700

    fixed alignment

--- cachematrix.R
+++ cachematrix.R
@@ -8,17 +8,17 @@
 ## that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-		i <- NULL
-		set <- function(y) {
-				x <<- y
-				i <<- NULL
-		}
-		get <- function() x
-		setinv <- function(inv) i <<- inv
-		getinv <- function() i
-		list(set = set, get = get,
-			 setinv = setinv,
-			 getinv = getinv)
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) i <<- inv
+        getinv <- function() i
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 ## cacheSolve function computes the inverse of the special "matrix"
@@ -27,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then the cacheSolve will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-		i <- x$getinv()
+        i <- x$getinv()
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
